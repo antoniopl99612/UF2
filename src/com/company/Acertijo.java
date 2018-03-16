236d82b2c2c7273c57e7f17cc1c53d0d06f50585
Antonio Pintado <antoniopl99612@gmail.com>
2018-03-16 19:34:27 +0100

    clase Adivinanzas

--- src/com/company/Acertijo.java
+++ src/com/company/Acertijo.java
@@ -1,21 +1,10 @@
 package com.company;
 
-import java.util.Scanner;
 
 public class Acertijo {
+    String pregunta;
+    String respuesta;
 
-int respuesta;
 
-    void acertijo1 (){
-        System.out.println("-----------------------------\n" +
-                "Braintrainging :: Acertijo\n" +
-                "\n" +
-                "Cuando yo tenia 6 años mi hermano tenia la mitad ahora tengo 70, cuantos años tengo?\n" +
-                "\n" +
-                "a) 35\n" +
-                "b) 64\n" +
-                "c) 55\n");
-        Scanner respuesta;
 
-    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+    }
+
+    void acertijo1 (){
+        System.out.println("-----------------------------\n" +
+                "Braintrainging :: Acertijo\n" +
+                "\n" +
+                "Cuando yo tenia 6 años mi hermano tenia la mitad ahora tengo 70, cuantos años tengo?\n" +
+                "\n" +
+                "a) 35\n" +
+                "b) 64\n" +
+                "c) 55\n");
+        Scanner respuesta;
+
     }
 }
