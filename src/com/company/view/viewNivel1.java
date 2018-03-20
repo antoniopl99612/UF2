537dfef44fdb03376f264ddc350f8a98c8e76803
Antonio Pintado <antoniopl99612@gmail.com>
2018-03-20 19:35:50 +0100

    clase Adivinanzas

---
+++ src/com/company/view/viewNivel1.java
@@ -0,0 +1,13 @@
+package com.company.view;
+
+public class viewNivel1 {
+
+System.out.println("-----------------------------\n" +
+        "Braintrainging :: Acertijo\n" +
+        "\n" +
+        "Cuando yo tenia 6 años mi hermano tenia la mitad ahora tengo 70, cuantos años tengo?\n" +
+        "\n" +
+        "a) 35\n" +
+        "b) 64\n" +
+        "c) 55\n");
+}
