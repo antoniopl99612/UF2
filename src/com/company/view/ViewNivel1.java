ac81a190e5666ca71c5c1be936a1367be1981491
Antonio Pintado <antoniopl99612@gmail.com>
2018-04-13 19:56:13 +0200

    clase Adivinanzas

--- src/com/company/view/ViewNivel1.java
+++ src/com/company/view/ViewNivel1.java
@@ -3,9 +3,10 @@ package com.company.view;
 public class ViewNivel1 {
 
 public void mostrar() {
-    System.out.println("-----------------------------\n" +
-            "Braintrainging - Acertijo\n" +
+    System.out.println("------------------------------------------------------\n" +
+            "              Braintrainging - Acertijo\n" +
             "\n" +
+            "------------------------------------------------------\n" +
             "Cuando yo tenia 12 años mi hermano tenia la mitad, ahora que tengo 70, ¿cuantos años tiene mi hermano?\n" +
             "\n" +
             "a) 35\n" +
