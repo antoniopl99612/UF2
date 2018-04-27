9f64055924a54cae974ac80ed655a0f19f4965a4
Antonio Pintado <antoniopl99612@gmail.com>
2018-04-27 19:50:48 +0200

    actualizacion

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("¿Que juego quieres jugar?\n" +
+                "a) Acertijos");
+        Scanner scanner = new Scanner(System.in);
+        String respuesta = scanner.nextLine();
+        if (respuesta == "a"){
+            ;
+        }
     }
 }
