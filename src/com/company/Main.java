ea46a28ee9384160890c0aa780d45d7c6d499423
Antonio Pintado <antoniopl99612@gmail.com>
2018-05-04 20:06:31 +0200

    merge total

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,33 +1,10 @@
 package com.company;
 
-import com.company.acertijo.view.AcertijoMain;
-import com.company.ordenaletras.view.OrdenaLetrasMain;
-
-import java.util.Scanner;
+import com.company.main.view.MenuPrincipal;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("------------------------------------------------------\n" +
-                "           BIENVENIDO AL JUEGO: BRAINTRAINING              \n"+
-                "------------------------------------------------------\n");
-        System.out.println("¿Que juego quieres jugar?\n" +
-                "a) Acertijos\n" +
-                "b) Ordenar letras\n" +
-                "c) Capitales\n" +
-                "d) Relaciona");
-        Scanner scanner = new Scanner(System.in);
-        String respuesta = scanner.nextLine();
-        if (respuesta.equals("a")){
-            new AcertijoMain().show();
-        }else if (respuesta.equals("b")){
-            new OrdenaLetrasMain().show();
-        }else if (respuesta == "c"){
-            ;
-        }else if (respuesta == "d"){
-            ;
-        }else {
-            System.out.println("Letra incorrecta");
-        }
+        new MenuPrincipal().show();
     }
 }
---
+++ src/com/company/main/view/MenuPrincipal.java
@@ -0,0 +1,32 @@
+package com.company.main.view;
+
+import com.company.acertijo.view.AcertijoMain;
+import com.company.ordenaletras.view.OrdenaLetrasMain;
+
+import java.util.Scanner;
+
+public class MenuPrincipal {
+    public void show(){
+        System.out.println("------------------------------------------------------\n" +
+                "           BIENVENIDO AL JUEGO: BRAINTRAINING              \n"+
+                "------------------------------------------------------\n");
+        System.out.println("¿Que juego quieres jugar?\n" +
+                "a) Acertijos\n" +
+                "b) Ordenar letras\n" +
+                "c) Capitales\n" +
+                "d) Relaciona");
+        Scanner scanner = new Scanner(System.in);
+        String respuesta = scanner.nextLine();
+        if (respuesta.equals("a")){
+            new AcertijoMain().show();
+        }else if (respuesta.equals("b")){
+            new OrdenaLetrasMain().show();
+        }else if (respuesta == "c"){
+            ;
+        }else if (respuesta == "d"){
+            ;
+        }else {
+            System.out.println("Letra incorrecta");
+        }
+    }
+}
