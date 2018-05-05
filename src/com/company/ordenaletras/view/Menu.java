522dd78964406b6f4d52729455cff315d08f1385
guille31 <37223600+guille31@users.noreply.github.com>
2018-05-05 18:09:01 +0200

    Update Menu.java

--- src/com/company/ordenaletras/view/Menu.java
+++ src/com/company/ordenaletras/view/Menu.java
@@ -1,30 +1,26 @@
-package com.company.ordenaletras.view;
-
+package com.company.View;
 import java.util.Scanner;
-
 public class Menu {
     public void mostrarTitulo(){
         System.out.println("\n++++++++++++_+++++++++++++++++++++++_+++++++++++++++++++++++_+++++++++++++++++++++++_+++++++++++");
         System.out.println("\n                           ¡¡BIENVENIDO!! AL JUEGO ORDENA LAS LETRAS\n");
         System.out.println("++++++++++++_+++++++++++++++++++++++_+++++++++++++++++++++++_+++++++++++++++++++++++_+++++++++++\n");
     }
+
     public void mostrarDescripcion() {
         System.out.println("Descripción: Este juego es distinto a todos los anteriores.Dispones de diversas letras por las que\ntendrás que descubrir su orden para formar palabras coherentes. \n\n");
     }
-        public void mostrarMenu(){
 
-        System.out.println("a) Nivel1");
-        System.out.println("b) Exit\n");
+    public String mostrarMenu(){
+
+        System.out.println("a) Nivel 1");
+        System.out.println("b) Nivel 2");
+        System.out.println("c) Nivel 3");
+        System.out.println("d) Exit\n");
         Scanner scanner = new Scanner(System.in);
         String opcion = scanner.nextLine();
-        if ("a".equals(opcion)) {
-            PantallaNivel1 pantallaNivel1 = new PantallaNivel1();
-            pantallaNivel1.mostrarN1();
-        } else if ("b".equals(opcion)) {
-            System.out.println("ADIOS, HAS SALIDO DEL JUEGO");
-        }
-    }
+
+        return opcion;
     }
 
-
-
+}
