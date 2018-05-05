b9017705535db20f0437adfef35e8c41b7c805f0
guille31 <37223600+guille31@users.noreply.github.com>
2018-05-05 18:16:24 +0200

    Create PantallaSiguienteNivel.java

---
+++ src/com/company/ordenaletras/view/PantallaSiguienteNivel.java
@@ -0,0 +1,23 @@
+package com.company.View;
+
+public class PantallaSiguienteNivel {
+    public void mostrar() {
+        MenuContinuarNivel menuContinuarNivel = new MenuContinuarNivel();
+        menuContinuarNivel.mostrarContinuar();
+        String opcion2 = menuContinuarNivel.mostrarContinuar();
+
+        if ("a".equals(opcion2)) {
+            PantallaNivel2 pantallaNivel2 = new PantallaNivel2();
+            pantallaNivel2.mostrar();
+
+        } else if ("b".equals(opcion2)) {
+            PantallaNivel1 pantallaNivel1 = new PantallaNivel1();
+            pantallaNivel1.mostrar();
+            pantallaNivel1.mostrarPalabra1();
+            pantallaNivel1.mostrarPalabra1();
+            pantallaNivel1.mostrarPalabra2();
+        } else if ("c".equals(opcion2)) {
+            System.out.println("ADIOS, HAS SALIDO DEL JUEGO");
+        }
+    }
+}
