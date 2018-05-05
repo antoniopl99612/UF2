0362f0c506ac94ed88e2c6d265d5580d801daa35
guille31 <37223600+guille31@users.noreply.github.com>
2018-05-05 18:10:03 +0200

    Update PantallaNivel1.java

--- src/com/company/ordenaletras/view/PantallaNivel1.java
+++ src/com/company/ordenaletras/view/PantallaNivel1.java
@@ -1,20 +1,52 @@
-package com.company.ordenaletras.view;
+package com.company.View;
+import com.company.Manager.ManagerPalabras;
 
-
-import com.company.ordenaletras.model.Palabras;
+import java.util.Scanner;
 
 public class PantallaNivel1 {
+    public void mostrar() {
+        System.out.println("\n**************");
+        System.out.println("    NIVEL 1");
+        System.out.println("**************\n");
 
 
-    public void mostrarN1() {
-
-       Menu menu = new Menu();
-       Palabras palabras = new Palabras();
-       menu.mostrarTitulo();
-       menu.mostrarDescripcion();
-       menu.mostrarMenu();
-       palabras.mostrarPalabras();
 
     }
-}
+        public String mostrarPalabra1 () {
+            ManagerPalabras managerPalabras = new ManagerPalabras();
 
+            Scanner scanner = new Scanner(System.in);
+            String palabra1 = managerPalabras.cogerPalabra();
+            String respuesta1 = "";
+            palabra1 = managerPalabras.desordenar(palabra1);
+            System.out.println(palabra1);
+            respuesta1 = scanner.nextLine();
+            return respuesta1;
+        }
+
+        public String mostrarPalabra2 () {
+            ManagerPalabras managerPalabras = new ManagerPalabras();
+            Scanner scanner = new Scanner(System.in);
+            String respuesta2 = "";
+            String palabra2 = managerPalabras.cogerPalabra();
+            palabra2 = managerPalabras.desordenar(palabra2);
+            System.out.println(palabra2);
+            respuesta2 = scanner.nextLine();
+            return respuesta2;
+        }
+
+
+        public String mostrarPalabra3 () {
+            ManagerPalabras managerPalabras = new ManagerPalabras();
+            Scanner scanner = new Scanner(System.in);
+            String respuesta3 = "";
+            String palabra3 = managerPalabras.cogerPalabra();
+            palabra3 = managerPalabras.desordenar(palabra3);
+            System.out.println(palabra3);
+            respuesta3 = scanner.nextLine();
+            return respuesta3;
+
+        }
+
+
+    }
