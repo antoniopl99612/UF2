34069f275157e58d0ab840bee9354182f1cce001
guille31 <37223600+guille31@users.noreply.github.com>
2018-05-05 18:14:48 +0200

    Create MenuContinuarNivel.java

---
+++ src/com/company/ordenaletras/view/MenuContinuarNivel.java
@@ -0,0 +1,15 @@
+package com.company.View;
+
+import java.util.Scanner;
+
+public class MenuContinuarNivel {
+    public String mostrarContinuar(){
+        String opcion;
+        System.out.println("Tu puntuación es: ");
+        System.out.println("¿Deseas pasar al siguiente nivel o repetir el nivel actual? ");
+        System.out.println("siguiente[a] repetir [b] salir[c]");
+        Scanner scanner = new Scanner(System.in);
+        String opcion2 = scanner.nextLine();
+        return opcion2;
+    }
+}
