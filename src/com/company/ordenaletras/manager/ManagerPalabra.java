90c9807ff0b96a6883fe16750faab3a90e43c829
guille31 <37223600+guille31@users.noreply.github.com>
2018-05-05 18:06:01 +0200

    Update ManagerPalabra.java

--- src/com/company/ordenaletras/manager/ManagerPalabra.java
+++ src/com/company/ordenaletras/manager/ManagerPalabra.java
@@ -1,7 +1,44 @@
 package com.company.ordenaletras.manager;
+import java.util.Random;
 
-import com.company.ordenaletras.model.Palabras;
+public class ManagerPalabras {
+    String valor;
+    String[][] valorN1 = {{"fresa", "ordenador", "calle", "nombre", "movil", "coche"},
+            {"torre", "correo", "camino", "nivel", "musica", "almuerzo"},
+            {"caer", "sudor", "lunes", "cuero", "bar", "orden"},
+            {"teclado", "reto", "juego", "perdedor", "mochila", "habitación"},
+            {"almacén", "mejor", "maratón", "balón", "deforme", "signos"},
+            {"podadora", "sidra", "pegar", "afeitadora", "calcetín", "oficina"}};
 
-public class ManagerPalabra {
-    public Palabras[] palabras= new Palabras[30];
+    public String cogerPalabra() {
+        Random random = new Random();
+
+        return valorN1[random.nextInt(5)][random.nextInt(6)];
+    }
+
+    public  String desordenar(String palabra) {
+        Random random = new Random();
+
+        boolean[] cogidas = new boolean[palabra.length()];
+
+        String palabraDesordenada ="";
+
+        for (int i = 0; i < palabra.length(); i++) {
+
+            if (cogidas[i] != true) {
+
+                palabraDesordenada += palabra.charAt(random.nextInt(4));
+                cogidas[i] = true;
+
+            } else if (cogidas[i] == true) {
+
+            } else if (cogidas [i] != true) {
+                palabraDesordenada += palabra.charAt(random.nextInt(5));
+                cogidas[i] = true;
+            }
+        }
+
+        return palabraDesordenada;
+    }
 }
+
