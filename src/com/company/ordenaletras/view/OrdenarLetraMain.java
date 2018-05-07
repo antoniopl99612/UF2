f7bf25ff8a0bb743fb0860546a7a819efcffec8e
guille31 <37223600+guille31@users.noreply.github.com>
2018-05-07 16:35:50 +0200

    Create OrdenarLetraMain.java

---
+++ src/com/company/ordenaletras/view/OrdenarLetraMain.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import com.company.View.Menu;
+import com.company.View.MenuContinuarNivel;
+import com.company.View.PantallaElegirNivel;
+import com.company.View.PantallaNivel1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        PantallaElegirNivel pantallaElegirNivel = new PantallaElegirNivel();
+        pantallaElegirNivel.mostrar();
+
+
+    }
+}
